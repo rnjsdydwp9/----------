0e4646c732c682bf921864abb8225d8d6fbb378a
권용제 < >
2024-02-28 18:40:07 +0900

    re

--- oz-bootcamp-1-main/03-ballang/src/app/(root)/page.tsx
+++ oz-bootcamp-1-main/03-ballang/src/app/(root)/page.tsx
@@ -1,11 +1,45 @@
-import Form from "./_components/Form";
+"use client";
+
+import Page from "@/components/Page";
+import { useQuery } from "@tanstack/react-query";
+
+const getProductsUrl =
+  "https://port-0-express-server-17xco2nlsidlckv.sel5.cloudtype.app/products";
 
 function HomePage() {
+  const {
+    data: products,
+    error,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryFn: () =>
+      fetch(getProductsUrl)
+        .then((response) => response.json())
+        .then((data) => data.result),
+    queryKey: ["products"],
+  });
+
+  if (isError) {
+    return <Page title="홈">에러났어요...</Page>;
+  }
+
   return (
-    <div>
-      <h1>홈페이지</h1>
-      <Form />
-    </div>
+    <Page title="홈">
+      <div className="bg-red-50 h-[400px]">
+        이곳은 배너 영역~~ 쇼핑몰 배너 상상해 주세요~
+      </div>
+
+      <div>
+        <h2>전체 상품 미리보기 영역~</h2>
+
+        {isLoading
+          ? "로딩중 ..."
+          : products.map((product: any) => (
+              <div key={product.id}>{product.name}</div>
+            ))}
+      </div>
+    </Page>
   );
 }
 
--- oz-bootcamp-1-main/03-ballang/src/app/(root)/products/page.tsx
+++ oz-bootcamp-1-main/03-ballang/src/app/(root)/products/page.tsx
@@ -1,5 +1,42 @@
+"use client";
+
+import Page from "@/components/Page";
+import { useEffect, useState } from "react";
+
 function ProductsListPage() {
-  return <div>ProductsListPage</div>;
+  const getProductsUrl =
+    "https://port-0-express-server-17xco2nlsidlckv.sel5.cloudtype.app/products";
+
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch(getProductsUrl, { method: "GET" })
+      .then((response) => response.json())
+      .catch((e) => {
+        setIsError("에러 났어요...");
+        setIsLoading(false);
+      })
+      .then((data) => {
+        setProducts(data.result);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isError) {
+    return <Page title="홈">에러났어요...</Page>;
+  }
+
+  return (
+    <Page title="전체 상품">
+      {isLoading
+        ? "로딩중 ..."
+        : products.map((product: any) => (
+            <div key={product.id}>{product.name}</div>
+          ))}
+    </Page>
+  );
 }
 
 export default ProductsListPage;
